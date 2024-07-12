11e02cb0efef46fff2ff75789d6e8bdf433c934e
BryanRSummit <bryan@reddsummit.com>
2024-07-12 12:47:07 -0600

    Getting started, may have to use different forms for each component

--- app/page.js
+++ app/page.js
@@ -1,10 +1,17 @@
 import Image from "next/image";
+import  {NumForm}  from "@/components/ui/NumForm"
+
+import { z } from "zod"
+ 
+const formSchema = z.object({
+  username: z.string().min(2).max(50),
+})
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-
+        <NumForm/>
       </div>
     </main>
   );
